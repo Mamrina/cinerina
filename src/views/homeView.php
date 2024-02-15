265ba7c9270fc39bec79a7f1e7a5a0b5d93a310f
mari c <special_mc@mailo.com>
2024-02-15 18:10:26 +0100

    remove public layout whitout twig

--- src/views/homeView.php
+++ src/views/homeView.php
@@ -5,28 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenur sur CinéRina</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 
 <body>
-    <header>
-        <div id="header">
+    <header id="header">
+        <div class="wrap">
             <div>
-                <div class="container">
-                    <a href="../index.php"><img src="/public/images/logocinérina.png" alt="Ciné Rina"></a>
-                    <h1 class="over">CinéRina</h1>
+                <div>
+                    <a href=""><img src="public/images/logo_cinerina.png" alt="Ciné Rina"></a>
+                    <!-- <h1>CinéRina</h1> -->
                 </div>
             </div>
             <nav>
-                <ul id="header">
-                    <li class="size">
-                        <a href="index.php">Accueil</a>
+                <ul class="wrap">
+                    <li>
+                        <a href="">Accueil</a>
                     </li>
-                    <li class="size">
-                        <a href="main.php">Films</a>
+                    <li>
+                        <a href="">Films</a>
                     </li>
-                    <li class="size">
-                        <a href="login.php">Se Connecter</a>
+                    <li>
+                        <a href="">Se Connecter</a>
                     </li>
                 </ul>
             </nav>
@@ -34,7 +34,7 @@
     </header>
     <div">
         <nav>
-            <ul id="header" class="mini-nav">
+            <ul class="wrap">
                 <li><a href="">Aventure</a></li>
                 <li><a href="">Drame</a></li>
                 <li><a href="">Comédie</a></li>
